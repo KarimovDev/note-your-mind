0a9515a0f96dea2c890121c492df55e10e429e08
KarimovDev <kardimail@gmail.com>
2019-04-21 01:10:39 +0300

    chore: update description to readme.md

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 ## Backend server
 
 Run `node server.js` for a backend server on `http://localhost:3000/`. Cors is enabled while developing.
-Later backend will use as a production server for app plus as proxy to mongodb.
+Also the backend has already existed on `http://78.155.218.226:3000/`
+Later the backend will use as a production server for app plus as proxy to mongodb.
 
 ## Database
 
 In this app is used MongoDB server is runed on `mongodb://78.155.218.226:27017`. The database name is `nym`.
+To remake database use initMongoDb.js in the root of the project.
--- src/app/shared-modules/ui-kit.module.ts
+++ src/app/shared-modules/ui-kit.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
-import { MatSidenavModule, MatToolbarModule, MatDividerModule, MatListModule, MatCardModule, MatSnackBarModule } from '@angular/material';
-
-@NgModule({
-  exports: [
+import {
     MatSidenavModule,
     MatToolbarModule,
     MatDividerModule,
     MatListModule,
     MatCardModule,
-    MatSnackBarModule
-  ]
+    MatSnackBarModule,
+} from '@angular/material';
+
+@NgModule({
+    exports: [
+        MatSidenavModule,
+        MatToolbarModule,
+        MatDividerModule,
+        MatListModule,
+        MatCardModule,
+        MatSnackBarModule,
+    ],
 })
-export class UiKitModule { }
+export class UiKitModule {}
